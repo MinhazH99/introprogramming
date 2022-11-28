44c189f0b2aa442ea84cc47b7c65c2da98189469
Yulu Wang <wangyulu980131@gmail.com>
2022-11-28 15:25:06 +0000

    changes to emergency.py

--- emergency.py
+++ emergency.py
@@ -1,4 +1,3 @@
-import sqlite3
 import os
 emergencyFilename = 'emergency_profile.txt'
 
@@ -24,21 +23,18 @@ def emegerncy_profile():
             elif volunteer_option == "4":
                 search_profile()
             elif volunteer_option == "5":
-                create_multiple_profiles()
-            elif volunteer_option == "6":
-                show_profile()
+                show_all_profile()
 
 
 def profile_menu():
     print("")
     print("Edit Emergency Profile Menu")
-    print("1. Create New Emergency Profile")
-    print("2. Edit Existing Emergency Profile")
-    print("3. Delete Existing Emergency Profile")
-    print("4. Search Emergency Profile")
-    print("5. Create Multiple Emergency Profiles")
-    print("6. Show All Emergency Profiles")
-    print("0. Exit")
+    print("[1] Create New Emergency Profile")
+    print("[2] Edit Existing Emergency Profile")
+    print("[3] Delete Existing Emergency Profile")
+    print("[4] Search Emergency Profile")
+    print("[5] Show All Emergency Profiles")
+    print("[0] Exit")
 
 def create_profile():
     print("Create New Emergency Profile")
@@ -69,18 +65,18 @@ def create_profile():
     print("The emergency profile(s) has been created.")
 
 def modify_profile():
-    show_profile()
+    #show_profile()
     if os.path.exists(emergencyFilename):
         with open(emergencyFilename, 'r', encoding='utf-8') as rfile:
             profile_info = rfile.readlines()
     else:
         return
     searched_refugee_name = input("Please enter the name of the refugee that you're searching: ")
-    with open(emergencyFilename, 'w', encoding='utf-8') as wfile:
+    with open(emergencyFilename, 'a+', encoding='utf-8') as wfile:
         for refugee in profile_info:
             d = dict(eval(refugee))
             if d['refugee_name'] == searched_refugee_name:
-                print("Please confirm the information of the refugee: ")
+                print("Found the refugee's profile. Please modify the information of the refugee: ")
                 while True:
                     try:
                         d['refugee_name'] = input("Please enter the refugee's name: ")
@@ -94,17 +90,18 @@ def modify_profile():
                 wfile.write(str(d) + '\n')
                 print("Successfully updated the profile of the refugee.")
             else:
+                # If the refugee's profile is not found, create a new profile for the refugee. 
                 wfile.write(str(d) +'\n')
         answer = str(input("Continue to edit another emergency profile? Y/N \n"))
         # Bug to be fixed: when input is not y/Y, will still execute modify_profile()
-        if answer == 'Y' or 'y':
+        if answer == 'Y' or answer== 'y':
             modify_profile()
             
 
 def delete_profile():
     while True:
-        refugee_name = str(input("Please enter the name of the refugee that you want to delete : "))
-        if refugee_name != '':
+        delete_refugee_name = input("Please enter the name of the refugee that you want to delete : ")
+        if delete_refugee_name != '':
             if os.path.exists(emergencyFilename):
                 with open (emergencyFilename, 'r', encoding='utf-8') as rfile:
                     profile_info = rfile.readlines()
@@ -116,48 +113,80 @@ def delete_profile():
                 with open(emergencyFilename, 'a+', encoding='utf-8') as wfile:
                     d = {}
                     for profile in profile_info:
-                        dict(eval(profile))
-                        if d['refugee_name'] != refugee_name:
+                        dict(eval(profile))  # Convert String to dictionary
+                        # If the refugee is not found, create a new profile for the refugee 
+                        if d['refugee_name'] != delete_refugee_name: 
                             wfile.write(str(d) +'\n')
                         else:
                             flag = True
                     if flag:
-                        print(f"Successfullt deleted the emergency profile of {refugee_name}")
+                        print(f'Successfullt deleted the emergency profile of {delete_refugee_name}')
                     else:
-                        print(f"The emergency profile of {refugee_name} not found.")
+                        print(f'The emergency profile of {delete_refugee_name} is not found.')
             else:
                 print("No emergency profile found.")
                 break
             show_profile()
-            answer = input("Continue to delete other emergency profile? Y/N")
-            if answer == 'Y' or 'y':
+            answer = input("Continue to delete other emergency profile? Y/N \n")
+            if answer == 'Y' or answer == 'y':
                 continue
             else:
                 break
 
 
-
-
-
-
-
-
-
-
 def search_profile():
-    pass
+    profile_query = []
+    while True:
+        refugee_name = input("Please enter the refugee's name: ")
+        if os.path.exists(emergencyFilename):
+            with open (emergencyFilename, 'r', encoding='utf-8') as rfile:
+                profile = rfile.readlines()
+                for item in profile:
+                    d = dict(eval(item))
+                    if refugee_name != '':
+                        if d['refugee_name'] == refugee_name:
+                            profile_query.append(d)
+            #show_profile(profile_query)
+            #clear the list
+            profile_query.clear()
+            answer = input("Continue to search emergency profile? Y/N \n")
+            if answer == 'Y' or 'y':
+                continue
+            else:
+                break
+        else:
+            print("Emergency profile not found.")
+            return
 
-def create_multiple_profiles():
-    pass
 
-def show_profile():
-    pass
+def show_profile(lst):
+    if len(lst) == 0:
+        print("No result found.")
+        return
+    # Design the layout of the result
+    format_title = '{:^16}\t{:^16}\t{:^16}\t{:^16}'
+    print(format_title.format('refugee_name', 'camp_code', 'family_number', 'medical_condition'))
+    format_content = '{:^16}\t{:^16}\t{:^16}\t{:^16}'
+    for item in lst:
+        print(format_content.format(item.get('refugee_name'), item.get('camp_code')), item.get('family_number'), item.get('medical_condition'))
+
+def show_all_profile():
+    profile_lst = []
+    if os.path.exists(emergencyFilename):
+        with open (emergencyFilename, 'r', encoding='utf-8') as rfile:
+            profile = rfile.readlines()
+            for item in profile:
+                profile_lst.append(eval(item))
+            if profile_lst:
+                show_profile(profile_lst)
+    else:
+        print("No result found. ")
 
 def save_profile(list):
     try:
-        profile_txt=open(emergencyFilename, 'a', encoding='utf-8')
+        profile_txt=open(emergencyFilename, 'a+', encoding='utf-8')
     except:
-        profile_txt=open(emergencyFilename, 'w', encoding='utf-8')
+        profile_txt=open(emergencyFilename, 'a+', encoding='utf-8')
     for item in list:
         profile_txt.write(str(item)+'\n')
     profile_txt.close()
