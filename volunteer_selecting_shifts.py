3a6b422c36ec416d59b5f84157e30b7852065a04
d-larbi <116180451+d-larbi@users.noreply.github.com>
2022-12-13 16:28:17 +0000

    Updated shift selection and changed home to match

--- volunteer_selecting_shifts.py
+++ volunteer_selecting_shifts.py
@@ -1,206 +1,237 @@
 import pandas as pd
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 import csv
 
-# #Creating CSV files for day and night shifts - would make empty fields, most likely,
-# #or just populate with available dates, or null values or something
-# #explore dataframes
-# #using approach from - https://www.geeksforgeeks.org/writing-csv-files-in-python/
+class DuplicateError(Exception):
+    '''Raises an error when a user tries to sign up for an existing shift'''
+    pass
 
-#If we wanted auto-generation of possible shifts (could get complicated):
-
-#Unsuccessful attempt at unction to generate the next 30 days
-
-# def daterange(date1, date2):
-#     coming_dates = []
-#     for n in range(int((date2 - date1).days) + 1):
-#         print(int((date2 - date1).days) + 1)
-#         date_to_add = date1 + timedelta(n)
-#         print(date_to_add)
-#         print([date_to_add])
-#         coming_dates += [date_to_add]
-#         return coming_dates
-    
-
-#if [1]:
-
-#TODO: Get range of next 30 days
-
-#TODO: If signing up for more than one shift on same day: raise error "No two consecutive shifts" - watch out, for overnight shifts more complexity
-#Shouldn't be too bad - start time of one shift is end time of previous, i.e. relationship between current and previous/ current and next row always holds,
-#except for first and last rows
+class ConsecutiveShiftError(Exception):
+    '''Raises an error when a user tries to sign up for consecutive shifts'''
+    pass
 
 
-#command to check whether *any* row in dataframe has this row already
-# day_shifts_df = pd.read_csv('day_shifts.csv')
-# print(day_shifts_df.loc[[0, 1]])
+def check_duplicates(df, start_date, end_date):
+    """Checks if a user has already signed up for a given shift. 
+    I think we should band together all the validation functions for each file into one class to demonstrate OOP"""
+    if ((df['startdate'] == f'{start_date}') & (df['enddate'] == f'{end_date}')).any():
+        return True
+    elif ((df['startdate'] == f'{start_date}') & (df['enddate'] == f'{end_date}')).any():
+        pass
+    else: 
+        return False
 
-# night_shifts_df = pd.read_csv('day_shifts.csv')
+def check_successive_shifts(df, shift_type, start_date, end_date):
+    """Checks if a user has signed up for successsive shifts."""
+    if shift_type == '1':
+        #Case 1: a night shift ends before - its end date is this start date, and its time is 19:00 - 09:00
+        if ((df['enddate'] == f'{start_date}') & (df['time'] == '19:00 - 09:00')).any():
+            return True
+        #Case 2: a night shift starts after this shift - its end date is this start date, and its time is 19:00 - 09:00
+        elif ((df['startdate'] == f'{end_date}') & (df['time'] == '19:00 - 09:00')).any():
+            return True
+    elif shift_type == '2':
+        #Case 1: a day shift starts before - its end date is this start date, and its time is 09:00 - 19:00
+        if ((df['enddate'] == f'{start_date}') & (df['time'] == '09:00 - 19:00')).any():
+            return True
+        # #Case 2: a day shift starts after - its start date is this end date, and its time is 09:00 - 19:00
+        elif ((df['startdate'] == f'{end_date}') & (df['time'] == '09:00 - 19:00')).any():
+            return True
+    return False
 
-# print( ((day_shifts_df['Date'] == '2022-11-28')
-# & (day_shifts_df['Time'] ==  '09:00 - 19:00')
-# & (day_shifts_df ['Volunteer username'] == 'Volunteer1')).any())
+def too_many_volunteers():
+    pass
+    #TODO: elif row in file: If count(row[volunteer]) < 5: insert new row. 
+    #Else: return 'no slots available on this date. Please pick another' (max. number of volunteers present at a time)
 
+def write_to_file(new_shift):
+    #For volunteer_change_camp: load CampDetails csv to dataframe
+    '''Function for writing to new shifts to shifts.csv'''
+    filename = "shifts.csv"
+    with open(filename, 'a+') as csvfile: #closes file after use
+        #creating a CSV write object-
+        csvwriter = csv.writer(csvfile)
+        #writing the fields
+        csvwriter.writerow(new_shift)
+        print("New shift successfully selected!")
+    #Change counts in CampDetails.csv
+    #   #For each campID:"
+    #   #   #count number of occurrences of that campID in shifts.csv
+    #   #   #write the result to No. df['Volunteers']
+    #Write df to campDetails.csv
+    #Instead of incrementing counts one-by-one, performs fresh count each time - less likely to cause errors
 
-#for the time being, getting username of user manually
-#once integrated with login, should be retrieved automatically
+def view_shifts(df, user_df, username):
+    '''Function for a volunteer to view their current shifts'''
+    past_shifts_df = pd.DataFrame(columns = user_df.columns)
+    today_shifts_df = pd.DataFrame(columns = user_df.columns)
+    future_shifts_df = pd.DataFrame(columns = user_df.columns)
+    print(past_shifts_df)
 
-#get usernames from existing test_file.csv - make this a class variable/ global variable?
+    #Print all rows where current shifts are in 
+    shifts = user_df.loc[df['username'] == f'{username}']
+    print(shifts)
+    # for ind in shifts.index:
+    #     print(shifts.loc[ind])
 
-# class selectShifts:
-#     """Class containing methods for users to view and add shifts (at present, no option to remove them).
-#     Also contains utility methods for use within these main methods."""
+    if shifts.empty:
+        print('No shifts at present')
+    else: 
 
+        for ind in shifts.index:
+            a = shifts.loc[ind]
+            b = a.values.tolist()
+            b[0] = b[0].date()
+            b[1] = b[1].date()
+            b_series = pd.Series(b, index = user_df.columns)
+            
+            if b[0] < date.today():
+                past_shifts_df = past_shifts_df.append(b_series, ignore_index = True)
+            elif b[0] == date.today():
+                today_shifts_df = today_shifts_df.append(b_series, ignore_index = True)
+            elif b[0] > date.today():
+                future_shifts_df = future_shifts_df.append(b_series, ignore_index = True)
 
-#     #for raising exceptions - could probably achieve using datetime function instead
-#     valid_days = [i for i in range(1,32)]
-#     valid_months = [i for i in range(1,13)]
-#     valid_years = [2022, 2023]
+        if len(past_shifts_df.index) > 0:
+            print("Past shifts:")
+            print(past_shifts_df)
+            print('\n')
+        if len(today_shifts_df.index) > 0: 
+            print("Today's shifts:")
+            print(today_shifts_df)
+            print('\n')
+        if len(future_shifts_df.index) > 0: 
+            print("Future shifts:")
+            print(future_shifts_df)
+            print('\n')
 
-def list_of_existing_users():
-    '''Gets list of existing users from test_file.csv'''
-    username_list = []
-    test_file = open('test_file.csv', 'r')
-    for line in test_file:
-        part = line.split(',') # creates list of strings separated by / from the line
-        a = part[3]
-        username_list += [a]
-    return username_list
+def add_new_shift(shift_type, user_df, username):
+    '''Function for users to add a new shift'''
+    print("You will be asked for the details of the shift you are signing up for")
+        
+    date_input = input('Please enter a date (YYYY-MM-DD): ')
 
+    try:
+        # Convert the string input to datetime object, it will catch error if you enter a unvalid date like 2020-12-40
+        start_date = datetime.strptime(date_input, '%Y-%m-%d').date()
+        # Limit the date_input to dates between today and thirty days' time
+        now = date.today()
+        thirty_days_time = (date.today() + timedelta(30)) 
+        if not (now <= start_date <= thirty_days_time):
+            raise ValueError
+        # elif shift already selected:
+        #     pass
+        #raise an error for incorrect format
+        else:
+            print("The date you entered ->%f<- is valid", date_input)
+            # You can write the date_input to csv file.
+    except ValueError:
+        print(f"Date out of range, please enter date from {now} to {thirty_days_time}.")
+        availability_funcs(user)
 
+    if shift_type == '1':
+        print(f"Sign up for a shift on {start_date} from 09:00 to 19:00?")
+        print("No: [0]")
+        print("Yes: [1]")
+        decision = input("Please select an option: ").strip()
+    elif shift_type == '2':
+        print(f"Sign up for a shift from {start_date} at 19:00 to {start_date + timedelta(1)} at 09:00?")
+        print("No: [0]")
+        print("Yes: [1]")
+        decision = input("Please select an option: ").strip()
 
-#Use the following function to select editing of day shifts or night shifts. 
-#Based on that choice, affects the files edited by availability_funcs
+    if decision == '0':
+        pass #returns to availability_funcs, which is called after this function executes
+    elif decision == '1':
 
-def select_shift_type():
-    shift_type = {'Day shifts': 0, 'Night shifts': 1, 'Exit': 2}
-    action = int(input(f"Edit day shifts or night shifts?:\n\
-    Day shifts: [{shift_type['Day shifts']}]\n\
-    Night shifts: [{shift_type['Night shifts']}]\n\
-    Exit: [{shift_type['Exit']}] \n"))
-    
-    if action == 0:
-        choice = 'day'
-    elif action == 1:
-        choice = 'night'
-    elif action == 2:
-        print('Goodbye')
+        if shift_type == '1':
+            end_date = start_date
+        elif shift_type == '2':
+            end_date = start_date + timedelta(1)
+
+        new_shift = [f'{start_date}', f'{end_date}', '19:00 - 09:00', f'{username}']
+
+        try:
+            if check_duplicates(user_df, start_date, end_date):
+                raise DuplicateError
+            if check_successive_shifts(user_df, shift_type, start_date, end_date):
+                raise ConsecutiveShiftError
+            else:
+                write_to_file(new_shift)
+
+        except DuplicateError:
+            print("You have already signed up for this shift. Please select another")
+            availability_funcs(user)
+        except ConsecutiveShiftError:
+            print("You cannot sign up for consecutive shifts.")
+            availability_funcs(user)
+            
     else: #error handling
         pass
-    shifts_df = pd.read_csv(f'{choice}_shifts.csv')
-    # print(f"Opening '{choice}_shifts.csv'")
-    return shifts_df
-
-def write_to_day_file(dt, username):
-    '''Function for writing to day_shifts.csv, since the number of columns differs between the two'''
-    filename = "day_shifts.csv"
-    with open(filename, 'a+') as csvfile: #closes file after use
-        #creating a CSV write object-
-        csvwriter = csv.writer(csvfile)
-        #writing the fields
-        new_day_shift = [f'{dt}', '09:00 - 19:00', f'{username}']
-        csvwriter.writerow(new_day_shift)
-        print("New shift successfully selected!")
-
-#Currently not working 0 not sure of the issue, should be identical to day shifts
-def write_to_night_file(dt, username):
-    '''Function for writing to night_shifts.csv, since the number of columns differs between the two'''
-    filename = "night_shifts.csv"
-    next_dt = dt + timedelta(1)
-    with open(filename, 'a+') as csvfile: #closes file after use
-        #creating a CSV write object-
-        csvwriter = csv.writer(csvfile)
-        #writing the fields
-        new_night_shift = [f'{dt}', f'{next_dt}' '19:00 - 09:00', f'{username}']
-        csvwriter.writerow(new_night_shift)
-        print("New shift successfully selected!")
 
 
 def availability_funcs(user):
-    shifts_df = select_shift_type() #makes dataframe from different csv files depending on shift type selected (day or night)
-    '''Method for user to view current shifts or add new shifts'''
-    #again, would need to incorporate select_shift_type
+    """Method for user to view current shifts or add new shifts. Main menu for this section.
+    The code block after 'username = user' is in place so that only volunteers who have a camp
+    (i.e., not new users) can view or edit shifts."""
+
     username = user
-    options = {'View current shifts': 0, 'Add new shift': 1, 'Exit': 2}
-    # action = int(input(f"Please select one of the following personal details to change:\n\
-    # View current shifts: [{options['View current shifts']}]\n\
-    # Add new shift: [{options['Add new shift']}]\n\
-    # Exit: [{options['Exit']}] \n"))
-    action = int(input("Please select one of the following personal details to change:"))
-    print("[0] View current shifts")
-    print("[1] Add new shift")
-    print("[2] Exit")
 
-    if action == 0:
-        #Print all rows where current shifts are in 
-        shifts = shifts_df.loc[shifts_df['Volunteer username'] == f'{username}']
-        if shifts.empty:
-            print('No shifts at present')
-        else: 
-            print(shifts_df.loc[shifts_df['Volunteer username'] == f'{username}'])
-            
-        print('\n')
-        availability_funcs()
+    #Check to see if volunteer has chosen a camp yet
+    all_volunteers_df = pd.read_csv("volunteers_db.csv")
+    volunteer_df = all_volunteers_df.loc[all_volunteers_df['usernames'] == f'{username}']
+    check_nan = volunteer_df.isnull().values.any()
 
-    elif action == 1:
-        #Turn into a function so I can call the function repeatedly
-        #File should be for getting a date AND writing to CSV, call repeatedly in here until user leaves
-        #When user leaves, return to main function/ dict for (0) other actions within availability, (1) main menu, (2) exit
-        print("You will be asked for the details of the shift you are signing up for")
-        y = int(input("Please enter a year as a single integer: "))
-        m = int(input("Please enter a month as a single integer, with no leading 0's (e.g., '1', not '01): "))
-        d = int(input("Please enter a day as a single integer, with no leading 0's (e.g., '1', not '01): "))
+    if check_nan: #doesn't allow new users to use these functions
+        print("It seems like you have not yet signed up to a camp. Please do so before trying to view and edit your shifts.")
+    else:
 
-        try:
-            dt = date(y, m, d)
-        except ValueError:
-            print('Invalid date format') #add a line so the user has to retry until they exit or choose a valid date
-        
-        if len(shifts_df.columns) == 3: #then choice made has been day shifts
-            decision = int(input(f"Sign up for a shift on {dt} at 09:00 - 19:00?\n\
-            No: [0]\n\
-            Yes: [1]\n" ))
-            if decision == 0:
-                availability_funcs()
-            elif decision == 1:
-                write_to_day_file(dt, username)
-                availability_funcs()
+        print("Which type of shift would you like to interact with?")
+        print("[1] Day shifts")
+        print("[2] Night shifts")
+        print("[3] Return to main menu")
+        while True:
+            shift_type = input("Please select an option: ").strip()
+            if shift_type == '1':
+                break
+            elif shift_type == '2':
+                break
+            elif shift_type == '3':
+                print("Returning you now: ")
+                break
             else: #error handling
+                print("Please enter a valid input.")
+
+        custom_date_parser = lambda x: datetime.strptime(x, '%Y-%m-%d')   
+        df = pd.read_csv("shifts.csv", parse_dates = ['startdate', 'enddate'], date_parser = custom_date_parser)
+        user_df = df[df['username'] == f'{username}'] #dataframe of user's shifts
+
+        print("[1] View current shifts")
+        print("[2] Add new shift")
+        print("[3] Return to home screen")
+
+        user_input = input("Please select an option: ")
+
+        if user_input == '1':
+            view_shifts(df, user_df, username)
+            print('--------------------------------')
+            availability_funcs(user)
+
+        elif user_input == '2':
+            if False: #change to condition 'if volunteer has not signed up for a camp'
                 pass
-        elif len(shifts_df.columns) == 4: #then choice made has been night shifts
-            decision = int(input(f"Sign up for a shift from {dt} at 19:00 to {dt + timedelta(1)} at 09:00?\n\
-            No: [0]\n\
-            Yes: [1]\n" ))
-            if decision == 0:
-                availability_funcs()
-            elif decision == 1:
-                write_to_night_file(dt, username)
-                availability_funcs()
-            else: #error handling
-                pass
+            else:
+                add_new_shift(shift_type, user_df, username)
+                print('--------------------------------')
+                availability_funcs(user)
 
+        elif user_input == 3:
+            #return to Home menu
+            print("Goodbye")
+        else: #error handling
+            pass
 
-        #Extra features:
-        #if date before today:
-        #add to list of prior dates
-        #print ("prior dates: \n")
-        #print out each date in list
-        #else:
-        #print("Today onwards: \n")
-        #print out each date in list
-        #Ask user to select year, month, day
-        #If tuple is before today: print(date is in past)
-        #if tuple is today: print(can't book shifts for current date! Must give advance notice)
-        #If tuple is more than 30 days in advance: print("Can't book more than 30 days in advance")
-        #Else:
-        #Check if tuple in csv file
-        #If so: print("You have already signed up for this time. Pick another time?") If yes, restart function. If not, exit
-        #If not: add row to csv file. If done successfully, print appropriate message
-        pass
-    #TODO: if row[date] not in csv file: add row to file (assuming ≤ 30 days)
-    #TODO: elif row in file: If count(row[volunteer]) < 5: insert new row. Else: return 'no slots available on this date. Please pick another' (max. number of volunteers present at a time)
-
-    elif action == 2:
-        print("Goodbye")
-    else: #error handling
-        pass
+# for testing this page in isolation, define a specific user below
+# user = 'volunteer2'
+# username = user
+# availability_funcs(user)
