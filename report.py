debecbb73aa18ec09c0fd43fb8ea40a7fbc53eb9
Yulu Wang <wangyulu980131@gmail.com>
2022-12-15 15:00:51 +0000

    Report function is mainly done

--- report.py
+++ report.py
@@ -4,9 +4,16 @@ import sys
 import pandas as pd
 from tabulate import tabulate
 from datetime import date
+# Not sure how to get current volunteer's username yet, set a dummy data.
+global volunteer
+volunteer = "volunteer1"
 
 
 def report():
+    """
+    Extra feature: Volunteer can report issues (harassment, resources, equipment, and other) happening in camps to admin.
+    Admin will receive the report and grade the severity of the issue. 
+    """
     while True:
         report_menu()
         volunteer_option = str(input("Option: "))
@@ -47,8 +54,6 @@ def create_report():
     # volunteer,title,category,camp_id,message,report_time,severity
     while True:
         print("")
-        # Not sure how to get current volunteer's username yet, set a dummy data.
-        volunteer = "volunteer1"
         while True:
             camp_id = str(input("Please enter the camp id that you want to report: "))
             # if there's no input, ask again 
@@ -81,9 +86,9 @@ def create_report():
                         continue
                     message = str(input("Please enter the report content: "))
                     # volunteer,camp_id,category,title,message,report_time,severity
-                    report_time = date.today().strftime("%Y-%m-%d")
+                    report_date = date.today().strftime("%Y-%m-%d")
                     break
-                report = {'volunteer':volunteer, 'camp_id':camp_id, 'category':category, 'title':title, 'message':message, 'report_time':report_time}
+                report = {'volunteer':volunteer, 'camp_id':camp_id, 'category':category, 'title':title, 'message':message, 'report_time':report_date, 'severity':'Not graded yet'}
                 report_list.append(report)
                 break
             else: 
@@ -94,11 +99,12 @@ def create_report():
             continue
         else:
             print("\nHere's the report(s) you've created just now:")
-            print(tabulate(pd.DataFrame(report_list).fillna("None"), headers=["Volunteer Name", "Camp ID", "Category", "Title", "Message", "Report Time"], tablefmt='fancy_grid', showindex=False))
+            df = pd.DataFrame(report_list).fillna("None")
+            df.loc[df['severity'].isnull(), 'severity'] = "Not graded yet"
+            print(tabulate(df, headers=["Volunteer Name", "Camp ID", "Category", "Title", "Message", "Report Time", "Severity"], tablefmt='fancy_grid', showindex=False))
             break
 
-
-    # If volunteer doesn't input the refugee's name, the info won't be written to the csv file. 
+    # Put the report info into report.csv file. 
     if report_list:
         with open("report.csv", "a", newline='') as file:
             writer = csv.DictWriter(file, fieldnames=['volunteer', 'camp_id', 'category', 'title', 'message', 'report_time', 'severity'])
@@ -106,15 +112,56 @@ def create_report():
                 writer.writerow(report)
             print("The report(s) has been created.")
 
-
                         
       
 def delete_report():
-    pass
+    while True:
+        print("\n----------------------------------------------------------------------------------------------------------------------------------------")
+        delete_report_title = str(input("Please enter the title of the report that you want to delete : "))
+        df = pd.read_csv('report.csv')
+
+        # Search every report from the volunteer that contains the keyword
+        contains_keyword = df.loc[(df['title'].str.contains(delete_report_title, case=False)) & (df['volunteer'] == volunteer)]
+        if len(contains_keyword) != 0: 
+            # Print out the search result
+            print(tabulate(contains_keyword.fillna("None"), headers=["Volunteer Name", "Camp ID", "Category", "Title", "Message", "Report Date", "Report Time"], tablefmt='fancy_grid', showindex=True))
+            
+            exact_delete_report_title = input("Please ensure deletion by entering the full title of the report that you want to delete, enter exit to search the report title again: ")
+            # df.drop(df.index[df['title'] == exact_delete_report_title], inplace = True)
+            df.drop(df.index[df['title'] == exact_delete_report_title], inplace = True)
+            df.to_csv('report.csv', index = False)
+            if df.drop(df.index[df['title'] == exact_delete_report_title], inplace = True) != None:
+                answer = input("Deleted the report successfully. Continue to delete another report? Y/N \n")
+                if answer == 'Y' or answer == 'y':
+                    continue
+                else:
+                    break
+            else:
+                print("Nothing deleted, going back to search the report title again...")
+        else:
+            print("Report not found. ")
+            return
 
 def view_my_report():
-    pass
+    # Check if report.csv exists, if so, print all reports; if not, print "no result found"
+    if os.path.exists("report.csv"):
+        print("\n----------------------------------------------------------------------------------------------------------------------------------------")
+        print("Summary of your reports:")
+        df = pd.read_csv("report.csv", header=0)
+        my_report = df.loc[df['volunteer'] == volunteer]
+        my_report.loc[df['severity'].isnull(), 'severity'] = "Not graded yet"
+        print(tabulate(my_report.fillna("None"), headers=["Volunteer Name", "Camp ID", "Category", "Title", "Message", "Report Date", "Severity"], tablefmt='fancy_grid', showindex=False))
+    else:
+        print("No result found. ")
 
 def view_all_report():
-    pass
-create_report()
+    # Check if report.csv exists, if so, print all reports; if not, print "no result found"
+    if os.path.exists("report.csv"):
+        print("\n----------------------------------------------------------------------------------------------------------------------------------------")
+        print("Summary of all reports:")
+        df = pd.read_csv("report.csv", header=0)
+        df.loc[df['severity'].isnull(), 'severity'] = "Not graded yet"
+        print(tabulate(df.fillna("None"), headers=["Volunteer Name", "Camp ID", "Category", "Title", "Message", "Report Date", "Severity"], tablefmt='fancy_grid', showindex=False))
+    else:
+        print("No result found. ")
+report()
