6cc2ce4aee9926fb0663842eda00db8dbe2af9fd
Yulu Wang <wangyulu980131@gmail.com>
2022-12-16 14:10:19 +0000

    Merge branch 'main' of https://github.com/MinhazH99/introprogramming

--- mail-demo.py
+++ mail-demo.py
@@ -1,19 +1,25 @@
 import smtplib
 import pandas as pd
+import subprocess
+
+# implement pip as subprogress:
+subprocess.Popen(['pip3', 'install', 'secure-smtplib'])
+
 
 sys_email = "3.mergency.sys@gmail.com"
 sys_password = "mhtxjymibjyqlixb"
 def get_password():
     # TODO Don't know how to get the volunteer's username, set a dummy data 'volunteer2'. 
     volunteer = 'volunteer2'
-    df = pd.read_csv('volunteers_db.csv')
+    df = pd.read_csv('volunteers_db.csv', index_col = False)
     user_email = df.loc[(df['usernames'] == volunteer)]['email']
     print(user_email)
     # If the user entered the right email that they registered
     if user_email.empty == False:
-        # TODO type(password) is an object, how can I convert it into a string?
-        password = df.loc[(df['usernames'] == volunteer)]['password']
-        print(password)
+        user_df = df.loc[(df['usernames'] == volunteer)]
+        password_list = user_df['password'].values
+        password = str(password_list[0])
+
         # Set up the connection with the mail server
         with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
             smtp.ehlo()
@@ -22,8 +28,8 @@ def get_password():
             smtp.login(sys_email, sys_password)
 
             # Set up the email content as follows:
-            subject = "No reply: Your password for e-mergency management system"
-            body = f"Your password is: {password}"
+            subject = "<No reply> Your password for e-mergency management system"
+            body = f"Hi {volunteer},\nYou recently sent a request to view your password.\nYour password is: {password}.\nDon't show this email to anyone else."
             msg = f"Subject:{subject}\n\n{body}"
             smtp.sendmail(sys_email, user_email, msg)
             print(f"Your password is sent to your email: {user_email}, please take a look.")
